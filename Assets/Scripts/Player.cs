a33b46aafa9cd079ead6475ea9783ea8fe04d713
dvdfu <davidf1212@gmail.com>
2020-04-17 23:19:32 -0700

    Fire hydrant prefab and circular player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     // Consts
-    const float MAX_SPEED = 300;
+    const float MAX_SPEED = 900;
     const float WATER_SHOOT_INTERVAL = 0.05f;
+    const int WATER_AMMO_MAX = 200;
 
     // Member vars
     Vector2 speed = Vector2.zero;
+    int waterAmmo = WATER_AMMO_MAX;
 
     // Unity vars
     [SerializeField] GameObject waterPelletPrefab;
-    [SerializeField]
-    Rigidbody2D body;
+    [SerializeField] Rigidbody2D body;
 
     void Start() {
         StartCoroutine(ShootWaterRoutine());
@@ -26,12 +27,15 @@ public class Player : MonoBehaviour {
 
     IEnumerator ShootWaterRoutine() {
         while (true) {
-            float x = Input.GetAxisRaw("WaterHorizontal");
-            float y = Input.GetAxisRaw("WaterVertical");
-            if (x != 0 || y != 0) {
-                float angle = Mathf.Atan2(y, x);
-                WaterPellet waterPellet = Instantiate(waterPelletPrefab, transform.position + Vector3.up * 20, Quaternion.identity, transform.parent).GetComponent<WaterPellet>();
-                waterPellet.Shoot(angle);
+            if (waterAmmo > 0) {
+                float x = Input.GetAxisRaw("WaterHorizontal");
+                float y = Input.GetAxisRaw("WaterVertical");
+                if (x != 0 || y != 0) {
+                    float angle = Mathf.Atan2(y, x);
+                    WaterPellet waterPellet = Instantiate(waterPelletPrefab, transform.position + Vector3.up * 20, Quaternion.identity, transform.parent).GetComponent<WaterPellet>();
+                    waterPellet.Shoot(angle);
+                    waterAmmo--;
+                }
             }
             yield return new WaitForSeconds(WATER_SHOOT_INTERVAL);
         }
