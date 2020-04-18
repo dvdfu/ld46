7b0f0f7d779ceccab005f96bf6531737d006e27d
Hamdan <hamdan@hamdanjaveed.com>
2020-04-17 21:04:12 -0700

    Firetruck prefab
    Create firetruck prefab and add a sprite flipper component

---
+++ Assets/Scripts/SpriteFlipper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Flips sprites depending on their horizontal velocity
+public class SpriteFlipper : MonoBehaviour {
+    [SerializeField]
+    private float threshold = 10.0f;
+    [SerializeField]
+    private bool flipOtherWay = false;
+
+    [SerializeField]
+    private SpriteRenderer sprite;
+    [SerializeField]
+    private Rigidbody2D body;
+
+    // Update is called once per frame
+    void Update() {
+        if (body.velocity.x > threshold) {
+            sprite.flipX = flipOtherWay == false;
+        } else if (body.velocity.x < -threshold) {
+            sprite.flipX = flipOtherWay == true;
+        }
+    }
+}
