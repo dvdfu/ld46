c47966e2ac948369bd5a43af055c05c8004eb140
dvdfu <davidf1212@gmail.com>
2020-04-18 11:59:48 -0700

    Screen shake

---
+++ Assets/Scripts/Object/Explosion.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+    void Start() {
+        Camera.main.gameObject.GetComponent<CameraHelper>().Shake();
+    }
+}
--- Assets/Scripts/Utility/CameraHelper.cs
+++ Assets/Scripts/Utility/CameraHelper.cs
@@ -7,10 +7,30 @@ public class CameraHelper : MonoBehaviour {
     [SerializeField] Transform target;
 
     Vector3 velocity = Vector3.zero;
+    Coroutine shakeCoroutine = null;
+    Vector3 shakeVector = Vector3.zero;
+
+    public void Shake(float duration = 0.1f, float strength = 4) {
+        if (shakeCoroutine != null) {
+            StopCoroutine(shakeCoroutine);
+        }
+        shakeCoroutine = StartCoroutine(ShakeRoutine(duration, strength));
+    }
 
     void FixedUpdate() {
         Vector3 position = target.position;
         position.z = -10;
-        camera.transform.position = Vector3.SmoothDamp(camera.transform.position, position, ref velocity, 0.25f);
+        camera.transform.position = Vector3.SmoothDamp(camera.transform.position, position, ref velocity, 0.25f) + shakeVector;
+    }
+
+    IEnumerator ShakeRoutine(float duration, float strength) {
+        float t = 0;
+        while (t < duration) {
+            t += Time.deltaTime;
+            shakeVector = MathUtils.PolarToCartesian(Random.value * 360, strength);
+            yield return null;
+        }
+        shakeVector = Vector3.zero;
+        shakeCoroutine = null;
     }
 }
