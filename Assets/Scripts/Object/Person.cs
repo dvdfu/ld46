89b8da09ad14f9001514ce457efe774fee4c392e
dvdfu <davidf1212@gmail.com>
2020-04-18 15:06:03 -0700

    People running script

---
+++ Assets/Scripts/Object/Person.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour {
+    [SerializeField] Rigidbody2D body;
+    [SerializeField] Sprite8Directional sprite8Directional;
+
+    float angle;
+
+    void Start() {
+        angle = Random.value * 360;
+        StartCoroutine(RunRoutine());
+    }
+
+    void FixedUpdate() {
+        body.AddForce(MathUtils.PolarToCartesian(angle, 40));
+    }
+
+    void LateUpdate() {
+        sprite8Directional.SetAngle(angle);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        angle = 180 - angle;
+    }
+
+    IEnumerator RunRoutine() {
+        while (true) {
+            yield return new WaitForSeconds(1 + Random.value);
+            angle = angle + Random.Range(-45, 45);
+        }
+    }
+}
