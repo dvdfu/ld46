7d2c0aa59dfca5641b4b949cbb5bff4311f03121
dvdfu <davidf1212@gmail.com>
2020-04-17 21:03:42 -0700

    Water pellet prefab

---
+++ Assets/Scripts/Sprinkler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprinkler : MonoBehaviour {
+    [SerializeField] GameObject waterPelletPrefab;
+
+    void Start() {
+        StartCoroutine(SprinkleRoutine());
+    }
+
+    IEnumerator SprinkleRoutine() {
+        while (true) {
+            Instantiate(waterPelletPrefab, transform.parent);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ Assets/Scripts/WaterPellet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPellet : MonoBehaviour {
+    [SerializeField] Rigidbody2D body;
+
+    void Start() {
+        body.velocity = new Vector2(Random.value, 1) * 200;
+    }
+}
