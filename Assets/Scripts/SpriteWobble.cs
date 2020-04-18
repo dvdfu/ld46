dee5df6563ee4e9f8e5d4be1f062e17716554ece
dvdfu <davidf1212@gmail.com>
2020-04-18 00:02:43 -0700

    Sprite wobble behaviour

---
+++ Assets/Scripts/SpriteWobble.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteWobble : MonoBehaviour {
+    const float PERIOD = 0.5f;
+
+    [SerializeField] Transform wobbleTransform;
+
+    void Start() {
+        StartCoroutine(WobbleRoutine());
+    }
+
+    IEnumerator WobbleRoutine() {
+        float t = 0;
+        float phase, scale;
+        while (true) {
+            t += Time.deltaTime;
+            phase = (t % 1) * Mathf.PI * 2;
+            scale = 1 + Mathf.Sin(phase / PERIOD) / 10;
+            transform.localScale = new Vector3(scale, 1 / scale);
+            yield return null;
+        }
+    }
+}
