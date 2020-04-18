2040b42c7f6c3a5326e2ae9a13b23e774c3c6011
dvdfu <davidf1212@gmail.com>
2020-04-17 20:31:18 -0700

    Top-down sortable behaviour

---
+++ Assets/Scripts/TopDownSortable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownSortable : MonoBehaviour {
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+    void LateUpdate() {
+        spriteRenderer.sortingOrder = -Mathf.FloorToInt(transform.position.y);
+    }
+}
