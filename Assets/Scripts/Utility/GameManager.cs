82b96fe4ba385b008da2a42fc28b3f00a9eabbaa
dvdfu <davidf1212@gmail.com>
2020-04-18 12:03:00 -0700

    Restart scene with R key

--- Assets/Scripts/Utility/GameManager.cs
+++ Assets/Scripts/Utility/GameManager.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     void Start() {
         Application.targetFrameRate = 60;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("r")) {
+            SceneManager.LoadScene("Game");
+        }
     }
 }
