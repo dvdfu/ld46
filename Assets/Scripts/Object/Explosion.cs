edd25546f682605bbd87a572fdc1282b4d35f8e8
dvdfu <davidf1212@gmail.com>
2020-04-18 12:10:39 -0700

    Explosion spreads fire

--- Assets/Scripts/Object/Explosion.cs
+++ Assets/Scripts/Object/Explosion.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour {
     void Start() {
+        RaycastHit2D[] results = Physics2D.CircleCastAll(transform.position, 32, Vector2.zero, 0);
+        foreach (RaycastHit2D result in results) {
+            Flammable flammable = result.collider.gameObject.GetComponent<Flammable>();
+            if (flammable) {
+                flammable.SetOnFire();
+            }
+        }
         Camera.main.gameObject.GetComponent<CameraHelper>().Shake();
     }
 }
