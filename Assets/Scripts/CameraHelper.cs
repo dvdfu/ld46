fc78f370a767a6c4c36fa384650b8b3dae972ebe
dvdfu <davidf1212@gmail.com>
2020-04-18 00:41:28 -0700

    Map bounds and camera following

---
+++ Assets/Scripts/CameraHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHelper : MonoBehaviour {
+    [SerializeField] Camera camera;
+    [SerializeField] Transform target;
+
+    Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate() {
+        Vector3 position = target.position;
+        position.z = -10;
+        camera.transform.position = Vector3.SmoothDamp(camera.transform.position, position, ref velocity, 0.15f);
+    }
+}
