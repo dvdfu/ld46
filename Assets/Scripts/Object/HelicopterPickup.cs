9a12638e94bd604993170efb5632e24f606b6204
dvdfu <davidf1212@gmail.com>
2020-04-18 17:47:58 -0700

    Tweak heli

--- Assets/Scripts/Object/HelicopterPickup.cs
+++ Assets/Scripts/Object/HelicopterPickup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HelicopterPickup : MonoBehaviour {
-    void OnTriggerEnter2D(Collider2D collider) {
+    void OnTriggerStay2D(Collider2D collider) {
         Player player = collider.gameObject.GetComponent<Player>();
         if (player) {
             player.RescuePeople();
