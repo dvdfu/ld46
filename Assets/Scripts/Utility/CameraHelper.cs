ff2c97148e2cf5630f2bc5b45bf16d0417214ff9
Hamdan <hamdan@hamdanjaveed.com>
2020-04-19 02:07:58 -0700

    Extend map to be 4 blocks wide

--- Assets/Scripts/Utility/CameraHelper.cs
+++ Assets/Scripts/Utility/CameraHelper.cs
@@ -18,9 +18,9 @@ public class CameraHelper : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        Vector3 position = target.position / 2;
+        Vector3 position = target.position;
         position.z = -10;
-        camera.transform.position = Vector3.SmoothDamp(camera.transform.position, position, ref velocity, 0.25f) + shakeVector;
+        camera.transform.position = Vector3.SmoothDamp(camera.transform.position, position, ref velocity, 0.15f) + shakeVector;
     }
 
     IEnumerator ShakeRoutine(float duration, float strength) {
