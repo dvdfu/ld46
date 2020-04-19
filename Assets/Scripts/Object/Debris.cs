6ae9228012d4381dab9d16e0b788c655db44fc73
dvdfu <davidf1212@gmail.com>
2020-04-19 01:04:51 -0700

    Fires will randomly eject burning debris every 2-5 seconds

---
+++ Assets/Scripts/Object/Debris.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debris : MonoBehaviour {
+    [SerializeField] GameObject firePrefab;
+    [SerializeField] Rigidbody2D body;
+
+    public void OnExpire() {
+        // Instantiate(firePrefab, transform.position, Quaternion.identity, transform.parent);
+    }
+
+    void Start() {
+        float angle = 90 + Random.Range(-45, 45);
+        body.velocity = MathUtils.PolarToCartesian(angle, 200);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.GetComponent<Flammable>()) {
+            collision.gameObject.GetComponent<Flammable>().SetOnFire();
+        }
+        Destroy(gameObject);
+    }
+}
