0133d350e264bce0794f4cd8cc9d621cc1277921
dvdfu <davidf1212@gmail.com>
2020-04-19 18:49:25 -0700

    Title prompt and sounds

--- Assets/Scripts/Object/TitleScreen.cs
+++ Assets/Scripts/Object/TitleScreen.cs
@@ -7,7 +7,10 @@ using UnityEngine.UI;
 public class TitleScreen : MonoBehaviour {
     [SerializeField] RectTransform titleWhatThe;
     [SerializeField] RectTransform titleFiretruck;
+    [SerializeField] Text prompt;
     [SerializeField] Text subtitle;
+    [SerializeField] AudioClip thudSound;
+    [SerializeField] AudioClip startSound;
 
     void Start() {
         StartCoroutine(IntroRoutine());
@@ -18,10 +21,12 @@ public class TitleScreen : MonoBehaviour {
         float scale = 0;
         titleWhatThe.localScale = Vector2.zero;
         titleFiretruck.localScale = Vector2.zero;
+        prompt.color = Color.clear;
         subtitle.color = Color.clear;
 
         yield return new WaitForSeconds(1);
 
+        SoundManager.Play(thudSound);
         while (t < 0.5f) {
             scale = Easing.ElasticOut(t * 2);
             titleWhatThe.localScale = new Vector2(scale, scale);
@@ -30,6 +35,7 @@ public class TitleScreen : MonoBehaviour {
         }
         titleWhatThe.localScale = new Vector2(1, 1);
 
+        SoundManager.Play(thudSound);
         t = 0;
         while (t < 0.5f) {
             scale = Easing.ElasticOut(t * 2);
@@ -42,14 +48,18 @@ public class TitleScreen : MonoBehaviour {
         t = 0;
         while (t < 0.3f) {
             float alpha = t / 0.3f;
+            prompt.color = new Color(1, 1, 1, alpha);
             subtitle.color = new Color(0, 0, 0, alpha);
             t += Time.deltaTime;
             yield return null;
         }
+        prompt.color = Color.white;
         subtitle.color = Color.black;
 
         while (true) {
             if (Input.anyKey) {
+                SoundManager.Play(startSound);
+                yield return new WaitForSeconds(0.5f);
                 SceneManager.LoadScene("Game");
             }
             yield return null;
