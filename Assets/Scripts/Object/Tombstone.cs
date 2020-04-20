82def58d2c34220a446d28cfc8126215b7374f1c
dvdfu <davidf1212@gmail.com>
2020-04-19 23:02:57 -0700

    Death sound

---
+++ Assets/Scripts/Object/Tombstone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tombstone : MonoBehaviour {
+    [SerializeField] AudioClip deathSound;
+
+    void Start() {
+        SoundManager.Play(deathSound);
+    }
+}
