6e9e6c99d0efadfb2723490f9ba6d14f6cee1dd8
dvdfu <davidf1212@gmail.com>
2020-04-19 23:38:46 -0700

    Results screen

---
+++ Assets/Scripts/Object/ResultsScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultsScreen : MonoBehaviour {
+    [SerializeField] SessionData sessionData;
+    [SerializeField] Text peopleDied;
+    [SerializeField] Text peopleSave;
+    [SerializeField] Text thingsIgnited;
+    [SerializeField] Text thingsExtinguished;
+    [SerializeField] Text waterUsed;
+
+    void Start() {
+        peopleDied.text = sessionData.peopleDied.ToString();
+        peopleSave.text = sessionData.peopleSaved.ToString();
+        thingsIgnited.text = sessionData.unitsBurned.ToString();
+        thingsExtinguished.text = sessionData.unitsExtinguished.ToString();
+        waterUsed.text = (sessionData.waterUsed * 4).ToString() + " L";
+    }
+}
