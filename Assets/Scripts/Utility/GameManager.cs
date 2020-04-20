09d08927acf9c4aee27afaf333f46e0adfcfd696
dvdfu <davidf1212@gmail.com>
2020-04-20 10:59:18 -0700

    Restart in editor only

--- Assets/Scripts/Utility/GameManager.cs
+++ Assets/Scripts/Utility/GameManager.cs
@@ -16,11 +16,13 @@ public class GameManager : MonoBehaviour {
         playerData.Reset();
     }
 
+    #if UNITY_EDITOR
     void Update() {
         if (Input.GetKeyDown("r")) {
             SceneManager.LoadScene("Game");
         }
     }
+    #endif
 
     void FixedUpdate() {
         if (sessionData.time > Time.deltaTime) {
