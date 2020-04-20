959d6357524765b356ba96bf565d66a9f8c0e7f3
Hamdan <hamdan@hamdanjaveed.com>
2020-04-20 02:21:13 -0700

    Results summary with ratings

--- Assets/Scripts/Object/ResultsScreen.cs
+++ Assets/Scripts/Object/ResultsScreen.cs
@@ -11,6 +11,7 @@ public class ResultsScreen : MonoBehaviour {
     [SerializeField] Text thingsIgnited;
     [SerializeField] Text thingsExtinguished;
     [SerializeField] Text waterUsed;
+    [SerializeField] Text summary;
 
     void Start() {
         peopleDied.text = sessionData.peopleDied.ToString();
@@ -18,5 +19,69 @@ public class ResultsScreen : MonoBehaviour {
         thingsIgnited.text = sessionData.unitsBurned.ToString();
         thingsExtinguished.text = sessionData.unitsExtinguished.ToString();
         waterUsed.text = sessionData.waterUsed.ToString() + " L";
+
+        SessionData.Ratings r = sessionData.GetRating();
+        SessionData.Title t = sessionData.GetTitle();
+
+        summary.text = "Fire truck unit LDXLVI was immediately dispatched to the scene of the fire";
+
+        switch (r.finalRating) {
+            case SessionData.Rating.Bad:
+                summary.text += ", only to further worsen the situation. ";
+                break;
+            case SessionData.Rating.Average:
+                summary.text += ". ";
+                break;
+            case SessionData.Rating.Good:
+                summary.text += ", and handled the situation to the best of their ability. ";
+                break;
+            case SessionData.Rating.Perfect:
+                summary.text += ", and valiantly handled the situation. ";
+                break;
+        }
+
+        switch (r.peopleRating) {
+            case SessionData.Rating.Bad:
+                summary.text += "There were mass casualties ";
+                break;
+            case SessionData.Rating.Average:
+                summary.text += "There were a number of casualties";
+                break;
+            case SessionData.Rating.Good:
+                summary.text += "There were a few casualties ";
+                break;
+            case SessionData.Rating.Perfect:
+                summary.text += "There were very few casualties ";
+                break;
+        }
+
+        switch (r.unitRating) {
+            case SessionData.Rating.Bad:
+                summary.text += " and charred buildings everywhere.";
+                break;
+            case SessionData.Rating.Average:
+                summary.text += " and they did their best to extinguish buildings, although some did burn down.";
+                break;
+            case SessionData.Rating.Good:
+                summary.text += " and most buildings were extinguished.";
+                break;
+            case SessionData.Rating.Perfect:
+                summary.text += " and almost no building was left unextinguished.";
+                break;
+        }
+
+        switch (t) {
+            case SessionData.Title.None:
+                break;
+            case SessionData.Title.GrimReaper:
+                summary.text += " You have been awarded the <color=#DF3E23>Grim Reaper</color> distinction.";
+                break;
+            case SessionData.Title.Arsonist:
+                summary.text += " You have been awarded the <color=#DD6000>Arsonist</color> distinction.";
+                break;
+            case SessionData.Title.HydroHomie:
+                summary.text += " You have been awarded the <color=#249FDE>HydroHomie</color> distinction.";
+                break;
+        }
     }
 }
--- Assets/Scripts/Utility/SessionData.cs
+++ Assets/Scripts/Utility/SessionData.cs
@@ -13,6 +13,26 @@ public class SessionData : ScriptableObject {
     public int waterUsed;
     public float time;
 
+    public struct Ratings {
+        public Rating peopleRating;
+        public Rating unitRating;
+        public Rating finalRating;
+    }
+
+    public enum Rating {
+        Bad,
+        Average,
+        Good,
+        Perfect,
+    }
+
+    public enum Title {
+        None,
+        GrimReaper, // >50 kills
+        Arsonist, // 0 extinguished/no water used
+        HydroHomie, // >2000L
+    }
+
     public void Reset() {
         peopleSaved = 0;
         peopleDied = 0;
@@ -20,5 +40,55 @@ public class SessionData : ScriptableObject {
         unitsExtinguished = 0;
         waterUsed = 0;
         time = GAME_DURATION;
+    }
+
+    public Ratings GetRating() {
+        Ratings ratings = new Ratings();
+
+        if (peopleDied < 5) {
+            ratings.peopleRating = Rating.Perfect;
+        } else if (peopleDied < 10) {
+            ratings.peopleRating = Rating.Good;
+        } else if (peopleDied < 20) {
+            ratings.peopleRating = Rating.Average; 
+        } else {
+            ratings.peopleRating = Rating.Bad;
+        }
+
+        int burnedDown = unitsBurned - unitsExtinguished;
+        if (burnedDown < 3) {
+            ratings.unitRating = Rating.Perfect;
+        } else if (burnedDown < 10) {
+            ratings.unitRating = Rating.Good;
+        } else if (burnedDown < 20) {
+            ratings.unitRating = Rating.Average;
+        } else {
+            ratings.unitRating = Rating.Bad;
+        }
+
+        int totalRating = (int)ratings.peopleRating + (int)ratings.unitRating;
+        if (totalRating == 6) {
+            ratings.finalRating = Rating.Perfect;
+        } else if (totalRating > 3) {
+            ratings.finalRating = Rating.Good;
+        } else if (totalRating > 0) {
+            ratings.finalRating = Rating.Average;
+        } else {
+            ratings.finalRating = Rating.Bad;
+        }
+
+        return ratings;
+    }
+
+    public Title GetTitle() {
+        if (peopleDied > 50) {
+            return Title.GrimReaper;
+        } else if (unitsExtinguished == 0) {
+            return Title.Arsonist;
+        } else if (waterUsed > 2000) {
+            return Title.HydroHomie;
+        } else {
+            return Title.None;
+        }
     }
 }
